ccdad83e2af750b1d6e0f52c69080881ad5ab3d7
Moshu <asadshahid234@gmail.com>
2026-06-01 12:52:04 +0500

    feat: add shared Product and CartItem types

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  title: string;
+  cat: string;
+  img: string;
+  price: number;
+  description: string;
+  moq: number;
+}
+
+export interface CartItem {
+  product: Product;
+  quantity: number;
+  size: string;
+  customNotes: string;
+}
